67131520ad0aaa0b9cd60b6dc5cdf7bdf8fad9bb
karthikasd <karthikadevarajanksrce@gmail.com>
2017-06-13 10:33:20 +0530

    Add files via upload

---
+++ SumofN.java
@@ -0,0 +1,18 @@
+package guvi;
+
+import java.util.Scanner;
+
+public class SumofN {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int sum=0;
+		for(int i=0;i<=n;i++){
+			sum=sum+i;
+					}
+		System.out.println(sum);
+		sc.close();
+	}
+
+}
