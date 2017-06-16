cfc54d4933ccc697a4dfd80855297df8f21b7821
karthikasd <karthikadevarajanksrce@gmail.com>
2017-06-16 15:25:21 +0530

    Update and rename sortndelete.java to sortingndelete.java

--- sortingndelete.java
+++ sortingndelete.java
@@ -6,16 +6,16 @@ public class Sort {
 	public static void main(String[] args) {
 		int i=0;
 Scanner sc=new Scanner (System.in);
-int a=sc.nextInt();
-int b[]=new int[6];
-while(a!=0){
-b[i]=a%10;
-a=a/10;
+int x=sc.nextInt();
+int y[]=new int[6];
+while(x!=0){
+y[i]=x%10;
+x=x/10;
 i++;
 }
-Arrays.sort(b,0,b.length-1);
+Arrays.sort(y,0,y.length-1);
 
-System.out.println(b[0]*10+b[1]);
+System.out.println(y[0]*10+y[1]);
 sc.close();
 	}
 }
