0490ca2a5ca2c1d6272f91710842874a75f3db1c
karthikasd <karthikadevarajanksrce@gmail.com>
2017-07-13 14:11:18 +0530

    Add files via upload

---
+++ StarIncrease.java
@@ -0,0 +1,14 @@
+package string_12;
+
+public class StarIncrease {
+
+	public static void main(String[] args) {
+		for(int i=1;i<=5;i++){
+			for(int j=1;j<=i;j++)
+			{
+			System.out.print("*");		
+				}
+			System.out.println();
+		}
+		}
+}
