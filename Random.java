f5e9350772c8df169d01058db9fff546ad7d65dc
karthikasd <karthikadevarajanksrce@gmail.com>
2017-07-14 15:22:03 +0530

    Add files via upload

---
+++ Random.java
@@ -0,0 +1,8 @@
+package string_8;
+public class Random {
+	public static void main(String[] args) {
+		int a=10;
+		a=(int)(Math.random()*a);
+		System.out.println(a);
+	}
+}
---
+++ Rounding.java
@@ -0,0 +1,11 @@
+package string_8;
+
+public class Rounding {
+
+	public static void main(String[] args) {
+		double given=5.4;
+		System.out.println(Math.round((given*10)/10));
+		}
+	}
+
+
